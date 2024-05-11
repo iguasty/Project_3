7d1c15d536dfc321068f2f6ebf98bdf20be56dba
iguasty <127635798+iguasty@users.noreply.github.com>
2024-05-11 16:50:36 -0700

    Fixed xy coordinate output

--- driver.py
+++ driver.py
@@ -16,8 +16,10 @@ entry_box = tk.Entry(root)
 enter_button = tk.Button(root, text="Enter")
 name_label = tk.Label(root, text=f"Your character's name is {person.name}.")
 basket_num_label = tk.Label(root, text=f"The basket number is {basket.basket_num}.")
-basket.coordinate_set()
-basket_coord_label = tk.Label(root, text=f"The basket coordinates are {basket.coordinates}.")
+basket.coordinate_set() #sets random xy coordinates
+x_coord = basket.coordinates[0] #gets x coordinate
+y_coord = basket.coordinates[1] #gets y coordinate
+basket_coord_label = tk.Label(root, text=f"The basket coordinates are: X={x_coord}  Y={y_coord}.")
 
 
 title_label.pack()
